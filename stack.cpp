5f489484a148e7cacc11a21fc70ab09413730138
eiralr <amberalsn@gmail.com>
2018-11-28 02:02:34 +0200

    added Push_(), Pop_() functions

--- stack.cpp
+++ stack.cpp
@@ -6,18 +6,35 @@ constexpr int max_num = 10;
 class Stack {
 	private:
 		int current_;
-		int * first_;
+		int * stack_arr_;
 	public:
 		Stack () {
-			first_ = new int[max_num];
+			stack_arr_ = new int[max_num];
 			current_ = 0;
 		}
 
 		~Stack() {
-			delete first_;
+			delete stack_arr_;
 		}
+
+		void Push_ (int new_value)
+		{
+			if (current_ < max_num) {
+				current_++;
+				stack_arr_[current_] = new_value;
+			}
+		}
+		
+		void Pop_ () 
+		{
+			if (current_ > 0) {
+				current_ --;
+			}
+		}
+
 };
 
 int main () {
 
+	return 0;
 }
